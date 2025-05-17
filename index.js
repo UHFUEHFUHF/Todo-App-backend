088be2aa25f31f3dc130f23d796f1cee7cbe7812
HiteshDubey <reenadubey7000@gmail.com>
2025-05-18 01:56:33 +0530

    Backend for Todo app mern

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import TodoRoutes from "./Routes/TodoRoutes.js"
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/todo", TodoRoutes)
+
+
+;(  async ()=>{
+try {
+  await  mongoose.connect(process.env.MONGODB_URI)
+  console.log("MONGODB connected")
+    app.on("error" , (error) =>{
+        console.log(error)
+        throw error
+    })
+
+} catch (error) {
+    console.log("connection error" , error)
+}
+})()
+
+
+app.listen(port , () =>{
+    console.log(`App is runnning on port ${port}`)
+})
